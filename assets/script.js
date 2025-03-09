4e72f87d12da471fd4c1c0e6454f394a09b01849
bagnukcompany <bagnukcompany@gmail.com>
2025-03-08 21:05:45 -0500

    фыв
    фвфв

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("Страница загружена. Добро пожаловать в BangukCompany!");
+    console.log("Сайт BangukCompany загружен!");
 });
